d9070eaebdca9c53b4542b56893735cb673a6a76
Fernanda Alves <fernanda@fernandalves.com>
2026-03-16 12:37:39 -0300

    Clean up repo - keep only redirect to Bragi

--- src/app/contexts/AdminContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-
-interface AdminActionsContextValue {
-  onHistorialClick?: () => void;
-}
-
-export const AdminActionsContext = createContext<AdminActionsContextValue>({});
-export const useAdminActions = () => useContext(AdminActionsContext);
